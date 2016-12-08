9fadc96faec90e9651c775272400f862ddc19ceb
Dylan <dylangernert@gmail.com>
2016-12-08 15:20:53 -0600

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,24 @@
 //numbers for coding message (you can change these to your likeing)
+var a = 1
+var b = 3
+var c = 6
+var d = 9
+var e = 0
+var f = 2
+var g = 4
+var h = 8
+var i = "b"
+var j = "d"
+var k = "e"
+var l = "f"
+var m = "a"
+var n = "c"
+var o = "p"
+var p = "q"
+var q = "r"
+var r = "w"
+var s = "v"
+var t = "l"
+var u = "m"
+var v = "p"
+console.log("test test");
